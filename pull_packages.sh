a9052b3fc932276388853603fde63b3a2d86e880
antman666 <945360554@qq.com>
2022-10-16 12:09:51 +0800

    update repo

--- pull_packages.sh
+++ pull_packages.sh
@@ -8,12 +8,12 @@ wget -c ${url}/gcc-fortran-12.2.1-1-x86_64.pkg.tar.zst
 wget -c ${url}/gcc-go-12.2.1-1-x86_64.pkg.tar.zst
 wget -c ${url}/gcc-libs-12.2.1-1-x86_64.pkg.tar.zst
 wget -c ${url}/gcc-objc-12.2.1-1-x86_64.pkg.tar.zst
-wget -c ${url}/glibc-2.36-4-x86_64.pkg.tar.zst
+wget -c ${url}/glibc-2.36-5-x86_64.pkg.tar.zst
 wget -c ${url}/lib32-gcc-libs-12.2.1-1-x86_64.pkg.tar.zst
-wget -c ${url}/lib32-glibc-2.36-4-x86_64.pkg.tar.zst
+wget -c ${url}/lib32-glibc-2.36-5-x86_64.pkg.tar.zst
 wget -c ${url}/libgccjit-12.2.1-1-x86_64.pkg.tar.zst
 wget -c ${url}/libtool-2.4.7-1-x86_64.pkg.tar.zst
-wget -c ${url}/llvm-15.0.1-1-x86_64.pkg.tar.zst
+wget -c ${url}/llvm-15.0.2-1-x86_64.pkg.tar.zst
 wget -c ${url}/lz4-1.1.9.4-1-x86_64.pkg.tar.zst
 wget -c ${url}/zstd-1.5.2-3-x86_64.pkg.tar.zst
-wget -c ${url}/zlib-1.1.2.12-2-x86_64.pkg.tar.zst
+wget -c ${url}/zlib-1.1.2.13-1-x86_64.pkg.tar.zst
