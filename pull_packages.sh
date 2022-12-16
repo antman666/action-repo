8c6c3a540ea0da98c5e560f3267cd5c14127e048
antman666 <945360554@qq.com>
2022-12-16 23:18:09 +0800

    update repo

--- pull_packages.sh
+++ pull_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 url="https://github.com/antman666/own_repo/releases/download/x86_64"
-wget -c ${url}/binutils-2.39-1-x86_64.pkg.tar.zst
+wget -c ${url}/binutils-2.39-4-x86_64.pkg.tar.zst
 wget -c ${url}/gcc-12.2.1-2-x86_64.pkg.tar.zst
 wget -c ${url}/gcc-ada-12.2.1-2-x86_64.pkg.tar.zst
 wget -c ${url}/gcc-fortran-12.2.1-2-x86_64.pkg.tar.zst
@@ -18,4 +18,6 @@ wget -c ${url}/llvm-15.0.6-1-x86_64.pkg.tar.zst
 wget -c ${url}/lz4-1.1.9.4-1-x86_64.pkg.tar.zst
 wget -c ${url}/zstd-1.5.2-3-x86_64.pkg.tar.zst
 wget -c ${url}/zlib-1.1.2.13-1-x86_64.pkg.tar.zst
+wget -c ${url}/perl-5.36.0-2-x86_64.pkg.tar.zst
+wget -c ${url}/minizip-1:1.2.13-1-x86_64.pkg.tar.zst
 wget -c ${url}/uksmd-git-0.0.0.r40.8f0349e-1-x86_64.pkg.tar.zst
