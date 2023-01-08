797a5aa3d99b16a2bf36b43a9dbaa5b8b6ae1ea1
antman666 <945360554@qq.com>
2023-01-08 14:05:46 +0800

    update repo

--- pull_packages.sh
+++ pull_packages.sh
@@ -2,17 +2,17 @@
 
 url="https://github.com/antman666/own_repo/releases/download/x86_64"
 wget -c ${url}/binutils-2.39-4-x86_64.pkg.tar.zst
-wget -c ${url}/gcc-12.2.1-2-x86_64.pkg.tar.zst
-wget -c ${url}/gcc-ada-12.2.1-2-x86_64.pkg.tar.zst
-wget -c ${url}/gcc-fortran-12.2.1-2-x86_64.pkg.tar.zst
-wget -c ${url}/gcc-go-12.2.1-2-x86_64.pkg.tar.zst
-wget -c ${url}/gcc-libs-12.2.1-2-x86_64.pkg.tar.zst
-wget -c ${url}/gcc-objc-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-12.2.1-3-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-ada-12.2.1-3-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-fortran-12.2.1-3-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-go-12.2.1-3-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-libs-12.2.1-3-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-objc-12.2.1-3-x86_64.pkg.tar.zst
 wget -c ${url}/glibc-2.36-5-x86_64.pkg.tar.zst
 wget -c ${url}/glib2-2.74.4-1-x86_64.pkg.tar.zst
-wget -c ${url}/lib32-gcc-libs-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/lib32-gcc-libs-12.2.1-3-x86_64.pkg.tar.zst
 wget -c ${url}/lib32-glibc-2.36-5-x86_64.pkg.tar.zst
-wget -c ${url}/libgccjit-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/libgccjit-12.2.1-3-x86_64.pkg.tar.zst
 wget -c ${url}/libtool-2.4.7-1-x86_64.pkg.tar.zst
 wget -c ${url}/llvm-15.0.6-2-x86_64.pkg.tar.zst
 wget -c ${url}/lz4-1.1.9.4-1-x86_64.pkg.tar.zst
